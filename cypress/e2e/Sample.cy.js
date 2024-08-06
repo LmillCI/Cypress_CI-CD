451e3077b096c454b44c3239d7fb561105ea3177
LmillCI <168177275+LmillCI@users.noreply.github.com>
2024-08-06 12:21:58 +0530

    Update Sample.cy.js

--- cypress/e2e/Sample.cy.js
+++ cypress/e2e/Sample.cy.js
@@ -4,13 +4,14 @@ describe('mysuite1', ()=>
     {
          cy.visit("https://opensource-demo.orangehrmlive.com/");
          cy.get("input[name='username']").type("Admin");
+        cy.screenshot('homepage');
          cy.get("input[name='password']").type("admin123");
          cy.get("button[type='submit']").click();  
     })
-    it('verify title -negative test', ()=>
-{
-    cy.visit("https://opensource-demo.orangehrmlive.com/");
-    cy.title().should('eq', 'OrangeHRM123');//assertion
+//     it('verify title -negative test', ()=>
+// {
+//     cy.visit("https://opensource-demo.orangehrmlive.com/");
+//     cy.title().should('eq', 'OrangeHRM123');//assertion
  
-})
+// })
     })
